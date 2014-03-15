e0261436fc482650ade9993ae66de30a2ca05681
FredLoney <loneyf@ohsu.edu>
2014-03-15 10:20:16 -0700

    Correct project name.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,11 +5,11 @@ autoclass_content = "both"
 templates_path = ['templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'qiprofile_rest'
+project = u'qiprofile-rest'
 copyright = u'2014, OHSU Knight Cancer Institute'
 pygments_style = 'sphinx'
 html_theme = 'qiprofile_rest_theme'
 html_theme_path = ['.']
 html_theme_options = dict(linkcolor='DarkSkyBlue', visitedlinkcolor='Navy')
 htmlhelp_basename = 'qiprofilerestdoc'
-html_title = "qiprofile_rest"
+html_title = "qiprofile-rest"
