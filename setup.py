1ad4fa4f5697ae9bda8b19c363a0766f44d12d2e
FredLoney <loneyf@ohsu.edu>
2014-05-22 12:39:06 -0700

    Clarify doc.

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from setuptools import (setup, find_packages)
 
 def version(package):
     """
-    Return package version as listed in `__version__` in `init.py`.
+    Return package version as listed in the `__init__.py` `__version__`
+    variable.
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
