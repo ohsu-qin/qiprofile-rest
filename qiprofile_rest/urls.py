0ca46ba25d47c46f0a48b159a181a4b95e549f76
FredLoney <loneyf@ohsu.edu>
2014-05-22 12:38:54 -0700

    Remove unused route patterns.

--- qiprofile_rest/urls.py
+++ qiprofile_rest/urls.py
@@ -3,24 +3,6 @@ from django.contrib import admin
 admin.autodiscover()
 from .routers import router
 
-# sess_patterns = patterns('',
-#     url(r'^/?$', views.session, name='view'),
-# )
-# 
-# sbj_patterns = patterns('',
-#     url(r'^/?$', views.subject, name='index'),
-#     url(r'^/session/(?P<session_number>\d+)',
-#         include(sess_patterns, namespace='session')),
-# )
-# 
-# app_patterns = patterns('',
-#     url(r'^/?$', SubjectList.as_view(), name='home'),
-#     url(r'^/(?P<collection_name>\w+)/(?P<subject_number>\d+)',
-#         include(sbj_patterns, namespace='subject')),
-#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-#     url(r'^/help/?$', views.help, name='help'),
-# )
-
 urlpatterns = patterns('',
     # The qiprofile REST URLs.
     url(r'', include(router.urls, namespace='api')),
