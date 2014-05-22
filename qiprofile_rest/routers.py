188fcafc6855da9cb7f946b3605ddca2bd20a0b8
FredLoney <loneyf@ohsu.edu>
2014-05-22 12:37:52 -0700

    Allow underscore or dash in URL.

--- qiprofile_rest/routers.py
+++ qiprofile_rest/routers.py
@@ -5,5 +5,5 @@ from .views import (UserViewSet, SubjectViewSet,
 router = routers.SimpleRouter()
 router.register(r'user', UserViewSet)
 router.register(r'subjects', SubjectViewSet)
-router.register(r'subject_detail', SubjectDetailViewSet)
-router.register(r'session_detail', SessionDetailViewSet)
+router.register(r'subject[_-]detail', SubjectDetailViewSet)
+router.register(r'session[_-]detail', SessionDetailViewSet)
