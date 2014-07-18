7f35b4ca93f291fce62948d9361f859a07ee4c0e
FredLoney <loneyf@ohsu.edu>
2014-07-18 10:23:44 -0700

    Remove Django files.

--- qiprofile_rest/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import (Subject)
-
-# Register the objects on the admin page.
-admin.site.register(Subject)
--- qiprofile_rest/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from .views import (UserViewSet, SubjectViewSet,
-                    SubjectDetailViewSet, SessionDetailViewSet)
-
-router = routers.SimpleRouter()
-router.register(r'user', UserViewSet)
-router.register(r'subjects', SubjectViewSet)
-router.register(r'subject[_-]detail', SubjectDetailViewSet)
-router.register(r'session[_-]detail', SessionDetailViewSet)
