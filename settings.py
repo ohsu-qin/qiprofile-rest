89dff1dcc193c0438a0e4bdbea96ce51eeaef345
FredLoney <loneyf@ohsu.edu>
2014-08-11 13:40:30 -0700

    Add run environment.

--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 # This file specifies the Eve configuration.
+import os
 
-MONGO_DBNAME = 'qiprofile'
+# The run environment default is production.
+# Modify this by setting the NODE_ENV environment variable.
+env = os.getenv('NODE_ENV') or 'production'
+# The MongoDB database.
+if env == 'production':
+  MONGO_DBNAME = 'qiprofile'
+else:
+  MONGO_DBNAME = 'qiprofile_test'
 
 # Even though the domain is defined by the Eve MongoEngine
 # adapter, a DOMAIN setting is required by Eve. This setting
