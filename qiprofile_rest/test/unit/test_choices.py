a1d0d662b1b4eb0b5354556a9ae6c87a15633856
FredLoney <loneyf@ohsu.edu>
2014-08-18 11:47:22 -0700

    Changed the choices method name.

--- qiprofile_rest/test/unit/test_choices.py
+++ qiprofile_rest/test/unit/test_choices.py
@@ -4,7 +4,7 @@ from qiprofile_rest import choices
 class TestChoices(object):
     def test_roman_range_choices(self):
         expected = [('I', 1), ('II', 2), ('III', 3), ('IV', 4)]
-        actual = choices.range_choices(1, 5)
+        actual = choices.roman_range_choices(1, 5)
         assert_equal(actual, expected)
 
 
