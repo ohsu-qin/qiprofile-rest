a0675bc3298d86ec82740e0822d622558fc7528d
FredLoney <loneyf@ohsu.edu>
2015-02-12 12:22:53 -0800

    Document __init__ separately.

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,6 @@ import sys
 import qiprofile_rest
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo']
-autoclass_content = "both"
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'qiprofile-rest'
@@ -11,3 +10,10 @@ copyright = u'2014, OHSU Knight Cancer Institute'
 pygments_style = 'sphinx'
 htmlhelp_basename = 'qiprofilerestdoc'
 html_title = "qiprofile-rest"
+
+def skip(app, what, name, obj, skip, options):
+    return False if name == "__init__" else skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
