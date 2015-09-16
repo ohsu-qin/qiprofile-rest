83d5a8e8b4d9776f54340e7e235953a58d1d0ef0
FredLoney <loneyf@ohsu.edu>
2015-09-15 18:51:18 -0700

    Remove OHSU reference.

--- qiprofile_rest/test/__init__.py
+++ qiprofile_rest/test/__init__.py
@@ -1,7 +1,7 @@
 """
 The qiprofile-rest unit tests.
 
-:Note: unlike the other OHSU QIN python projects, the qiprofile-rest
+:Note: unlike the other qi* python projects, the qiprofile-rest
   tests are contained within the main qiprofile-rest package. The test
   module is thus contained in the qiprofile-rest source distribution.
   This allows other python projects to call the qiprofile-rest seed
