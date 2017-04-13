393a2126b94b00b04bd6d2ef0e0d657dad1885ca
FredLoney <loneyf@ohsu.edu>
2017-04-13 11:19:51 -0700

    Bump version.

--- qirest/__init__.py
+++ qirest/__init__.py
@@ -1,6 +1,6 @@
 """The Quantitative Imaging Profile (*QiPr*) REST server."""
 
-__version__ = '6.2.11'
+__version__ = '6.2.12'
 """
 The one-based major.minor.patch version. Minor and patch
 version numbers begin at 1.
